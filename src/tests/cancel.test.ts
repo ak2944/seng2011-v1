5010bd41325427143f4abff9d3c634937b8ce540
Tom van Balkom <z5588874@ad.unsw.edu.au>
2025-03-13 19:47:42 +1100

    Test: completed basic tests for cancel

--- src/tests/cancel.test.ts
+++ src/tests/cancel.test.ts
@@ -7,8 +7,20 @@ beforeEach(() => {
 
 describe('api/despatchAdvice/cancel', () => {
     test('Working correctly', () => {
-        const response = requestCancel(id, 'Felt like it');
+        const response = requestCancel(id, 'Valid reason'); // TODO define valid reason
         expect(response.statusCode).toStrictEqual(200);
         expect(response.body).toStrictEqual({ message: expect.any(String) });
+    });
+
+    test('Invalid reason', () => {
+        const response = requestCancel(id, 'No');
+        expect(response.statusCode).toStrictEqual(400);
+        expect(response.body).toStrictEqual({ error: expect.any(String) });
+    });
+
+    test('Advice not found', () => {
+        const response = requestCancel(id + ' ', 'Valid reason');
+        expect(response.statusCode).toStrictEqual(404);
+        expect(response.body).toStrictEqual({ error: expect.any(String) });
     });
 });
