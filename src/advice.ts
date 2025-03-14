96eb02ab7eb064f128dfe12ac64eb5bad77fd6a8
Tom van Balkom <z5588874@ad.unsw.edu.au>
2025-03-14 12:05:16 +1100

    feat: Implemented logic for cancelAdvice

--- src/advice.ts
+++ src/advice.ts
@@ -1,4 +1,29 @@
-export function cancelAdvice(adviceId: string, reason: string) {
-    // Implement logic
+import { ADVICE_ID_INVALID, INVALID_REASON } from './errors';
+
+const DespatchAdvice = require('../DespatchAdvice');
+
+/**
+ * Cancels despatch advice given an ID and a reason
+ * @param { string } adviceId - the ID of the despatch advice
+ * @param reason - The reason for cancellation
+ * @returns { message: string } - A message stating the status of the cancellation
+ */
+export async function cancelAdvice(adviceId: string, reason: string) {
+    if (!isValidReason(reason)) throw INVALID_REASON;
+
+    const advice = await DespatchAdvice.findOne({ adviceId });
+    if (!advice) {
+        throw ADVICE_ID_INVALID;
+    }
+
+    advice.status = 'Cancelled';
+    advice.cancellationReason = reason;
+
+    await advice.save();
+
     return { message: 'Despatch Advice cancelled successfully' };
+}
+
+function isValidReason(reason: string) {
+    return (reason === 'Valid reason');
 }
--- src/errors.ts
+++ src/errors.ts
@@ -1,1 +1,2 @@
 export const INVALID_REASON = new Error('Cancellation reason');
+export const ADVICE_ID_INVALID = new Error('Advice ID is invalid');
