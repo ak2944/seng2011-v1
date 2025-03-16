6741a6e1a9cc0c4230a6bfc9ed30a0325c4bce4e
Tom van Balkom <z5588874@ad.unsw.edu.au>
2025-03-16 15:38:04 +1100

    feat: Finished implementation for cancel

--- src/tests/cancel.test.ts
+++ src/tests/cancel.test.ts
@@ -1,13 +1,46 @@
+import { getBody } from '../helpers';
 import { requestCancel } from '../requestWrap';
+import { ParsedOrder } from '../types/orderTypes';
+import { requestOrderParse } from './parse-order.test';
 
-let id: string;
+const validXml = `
+            <?xml version="1.0" encoding="UTF-8"?>
+            <Order xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+                xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+                xmlns="urn:oasis:names:specification:ubl:schema:xsd:Order-2">
+            <cbc:ID>AEG012345</cbc:ID>
+            <cbc:SalesOrderID>CON0095678</cbc:SalesOrderID>
+            <cbc:UUID>6E09886B-DC6E-439F-82D1-7CCAC7F4E3B1</cbc:UUID>
+            <cbc:IssueDate>2005-06-20</cbc:IssueDate>
+            </Order>
+        `;
+
+const validUserInputs = {
+    despatchId: '123456',
+    deliveredQuantity: '5',
+    backorderQuantity: '20',
+    backorderReason: 'Overstocked',
+    shipmentStartDate: '2005-06-21',
+    shipmentEndDate: '2005-06-28',
+    lotNumberID: '10',
+    lotExpiryDate: '2006-10-10',
+    despatchLineNote: 'none'
+};
+
+let parsedOrder: ParsedOrder;
+beforeAll(() => {
+    // Assumes requestOrderParse is export
+    parsedOrder = getBody(requestOrderParse(validXml));
+});
+
+const id = validUserInputs.despatchId;
 beforeEach(() => {
-    id = 'hi'; // TODO change to generate id properly
+    requestGenerate(parsedOrder, validUserInputs);
 });
 
 describe('api/despatchAdvice/cancel', () => {
     test('Working correctly', () => {
-        const response = requestCancel(id, 'Valid reason'); // TODO define valid reason
+        const response = requestCancel(id, 'Valid reason');
         expect(response.statusCode).toStrictEqual(200);
         expect(response.body).toStrictEqual({ message: expect.any(String) });
     });
