4d289c1b82e84c1ade4b1308c9789264f3fcd7b2
ak2944 <125517413+ak2944@users.noreply.github.com>
2025-04-07 21:40:32 +1000

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+console.log("🧪 Vite is building with env:", process.env.VITE_BACKEND_URL);
+
 export default defineConfig({
   plugins: [react()],
   test: {
